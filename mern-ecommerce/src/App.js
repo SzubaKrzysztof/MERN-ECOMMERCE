9a2d5fbb59a8ff84795a8d37cf7d78af454fae74
SzubaKrzysztof <szuba.christopher@gmail.com>
2020-11-04 16:09:44 +0100

    finishing layout of the homepage

--- mern-ecommerce/src/App.js
+++ mern-ecommerce/src/App.js
@@ -3,15 +3,18 @@ import { Container } from 'react-bootstrap'
 
 import Header from './components/Header';
 import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen.jsx';
 
 
 const App = () => {
   return (
     <>
       <Header/>
+      <main className='py-3'>
       <Container>
-      <main><h1> Welcome </h1></main>
+      <HomeScreen/>
       </Container>
+      </main>
       <Footer/>
     </>
   );
